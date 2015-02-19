9e55dd23a1b49e9b434357660baf7d79c2aab42d
fowl <phowl.mouth@gmail.com>
2015-02-19 02:51:44 -0600

    fixed cases on sdl_skeleton, fixed warnings

--- examples/sdl_skeleton.nim
+++ examples/sdl_skeleton.nim
@@ -1,33 +1,33 @@
 ## Bare-bones SDL2 example 
 import sdl2, sdl2/gfx 
 
-discard SDL_Init(INIT_EVERYTHING)
+discard sdl2.init(INIT_EVERYTHING)
 
 var 
-  window: PWindow
-  render: PRenderer
+  window: WindowPtr
+  render: RendererPtr
 
-window = CreateWindow("SDL Skeleton", 100, 100, 640,480, SDL_WINDOW_SHOWN)
-render = CreateRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+window = createWindow("SDL Skeleton", 100, 100, 640,480, SDL_WINDOW_SHOWN)
+render = createRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
 
 var
-  evt: TEvent
+  evt = Event(kind: QuitEvent)
   runGame = true
-  fpsman: TFPSmanager
+  fpsman: FpsManager
 fpsman.init
 
 while runGame:
-  while PollEvent(evt):
+  while pollEvent(evt):
     if evt.kind == QuitEvent:
       runGame = false
       break
   
   let dt = fpsman.getFramerate() / 1000
   
-  render.SetDrawColor 0,0,0,255
-  render.Clear
+  render.setDrawColor 0,0,0,255
+  render.clear
   
-  render.Present
+  render.present
   fpsman.delay
 
 destroy render
