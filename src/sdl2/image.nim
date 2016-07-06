cd6823e81630d9f4bf09717b01cd828bd76ce2d6
Peter Munch-Ellingsen <peterme@peterme.net>
2016-07-06 16:25:42 +0200

    Removed header from static compilation of image sub-module as it broke compilation on Android

--- src/sdl2/image.nim
+++ src/sdl2/image.nim
@@ -14,9 +14,7 @@ const
   IMG_INIT_TIF* = 0x00000004
   IMG_INIT_WEBP* = 0x00000008
 
-when defined(SDL_Static):
-  {.push header: "<SDL2/SDL_image.h>".}
-else:
+when not defined(SDL_Static):
   {.push callConv:cdecl, dynlib: LibName.}
 
 proc linkedVersion*(): ptr SDL_version {.importc: "IMG_Linked_Version".}
