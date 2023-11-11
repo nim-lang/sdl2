d512f6a9a4de488bcfc57a4106dff01ad8c0f522
Amjad Ben Hedhili <amjadhedhili@outlook.com>
2023-11-11 06:15:05 +0100

    Fix warnings (#155)
    * Fix csize warnings
    
    * Fix more warnings
    
    * Fix syntax error
    
    * update examples
    
    * Apply suggestions from code review
    
    * Update src/sdl2/gfx.nim
    
    ---------
    
    Co-authored-by: Andreas Rumpf <rumpf_a@web.de>

--- examples/pong.nim
+++ examples/pong.nim
@@ -155,7 +155,7 @@ proc drawScores(
   let
     color = color(255, 255, 255, 0)
     text = $scores.player & " : " & $scores.opponent
-    surface = ttf.renderTextSolid(font, text, color)
+    surface = ttf.renderTextSolid(font, cstring text, color)
     texture = renderer.createTextureFromSurface(surface)
 
   surface.freeSurface
